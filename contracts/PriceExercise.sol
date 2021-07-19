05f1dba1e83de513a1eaf6fec7e394bb4dcb726e
altugbakan <altugbakan@gmail.com>
2021-07-19 14:35:03 +0300

    Developer Bootcamp

---
+++ contracts/PriceExercise.sol
@@ -0,0 +1,105 @@
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract PriceExercise is ChainlinkClient {
+    bool public priceFeedGreater;
+    AggregatorV3Interface internal priceFeed;
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+
+    constructor(
+        address _oracle,
+        string memory _jobId,
+        uint256 _fee,
+        address _link,
+        address _priceFeed
+    ) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        if (_link == address(0)) {
+            setPublicChainlinkToken();
+        } else {
+            setChainlinkToken(_link);
+        }
+        // oracle = 0x2f90A6D021db21e1B2A077c5a37B3C7E75D15b7e;
+        // jobId = "29fa9aa13bf1468788b7cc4a500a45b8";
+        // fee = 0.1 * 10 ** 18; // 0.1 LINK
+        oracle = _oracle;
+        jobId = stringToBytes32(_jobId);
+        fee = _fee;
+    }
+
+    /*
+     * Requests the latest price of BTC/USD.
+     */
+    function requestPriceData() public returns (bytes32 requestId) {
+        Chainlink.Request memory request = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+
+        // Set the URL to perform the GET request on
+        request.add(
+            "get",
+            "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD"
+        );
+
+        // Set the path to find the desired data in the API response, where the response format is:
+        // {"RAW":
+        //   {"ETH":
+        //    {"USD":
+        //     {
+        //      "VOLUME24HOUR": xxx.xxx,
+        //     }
+        //    }
+        //   }
+        //  }
+        request.add("path", "RAW.BTC.USD.PRICE");
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int256 timesAmount = 10**18;
+        request.addInt("times", timesAmount);
+
+        // Sends the request
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+
+    /*
+     * Compares the BTC/USD price data requested from the cryptocompare API
+     * with the oracle price feed obtained from Chainlink.
+     */
+    function fulfill(bytes32 _requestId, int256 _price) public recordChainlinkFulfillment(_requestId) {
+        priceFeedGreater = getLatestPrice() > _price;
+    }
+
+    /*
+     * Converts string to Bytes32.
+     */
+    function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+
+    /*
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int256) {
+        (
+            uint80 roundID,
+            int256 price,
+            uint256 startedAt,
+            uint256 timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
